ac30fb24021f89da9daa9273d89e379998db0545
almon-s <almon.subba@gmail.com>
2023-01-04 13:09:29 +0800

    Update for previous commit too. Removed bs4 and just used selenium since the website was dynamic, had to emulate how a human would click instead. I tried just requests along with both bs4 and selenium but it was too simple. There was no way to access the remaining job desc. Realised I had to click the 'job cards' instead. Concluded making a function to collect everything I wanted was needed. The first iteration was simple, the latest one allows me to click through and scrape all the necesasry info. Try blocks were added to catch the sign up box and close it, it started popping up after you get signed out. Only used dataframe with jupyter, trying to figure that one out still.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
 import time 
 import pandas as pd
 from sklearn.datasets import load_iris
@@ -10,28 +11,33 @@ def python_jobs(num_jobs, check):
     url = 'https://www.glassdoor.com/Job/hong-kong-python-developer-jobs-SRCH_IL.0,9_IC2308631_KO10,26.htm?sortBy=date_desc'
     driver = webdriver.Chrome('Users/almonsubba/Downloads/chromedriver')
     driver.get(url)
-    data = load_iris()
     jobs = []
 
+    # If its true, will still be looking for jobs.
     while len(jobs) < num_jobs:
 
         time.sleep(4)
 
+        # For the sign up prompt, get rid of it.
         try:
             driver.find_element(By.CLASS_NAME, 'selected').click()
-        except:
-            time.sleep(.1)
+        except ElementClickInterceptedException:
+            pass
 
+        time.sleep(.1)
+
+        # Clicking the 'X' button.
         try:
             driver.find_element(By.CLASS_NAME, 'ModalStyle__xBtn___29PT9').click()
-        except:
-            time.sleep(.1)
+        except NoSuchElementException:
+            pass
 
 
-        #Clicking each job card button.
+        # Clicking each job card button.
         job_buttons = driver.find_elements(By.CLASS_NAME, 'jlGrid')
         for job_button in job_buttons:
             print("Loading...")
+            
             if len(jobs) >= num_jobs:
                 break
 
@@ -39,6 +45,7 @@ def python_jobs(num_jobs, check):
             time.sleep(1)
             collected_successfully = False
             
+            # Keeps searching unless.
             while not collected_successfully:
                 try:
                     company_name = driver.find_element(By.XPATH, '//div[@data-test="employerName"]').text
@@ -46,7 +53,7 @@ def python_jobs(num_jobs, check):
                     job_description = driver.find_element(By.XPATH, '//div[@class="jobDescriptionContent desc"]').text
                     collected_successfully = True
                 except:
-                    time.sleep(3)
+                    time.sleep(5)
 
             if check:
                 print("Job Title: {}".format(job_title))
@@ -57,8 +64,15 @@ def python_jobs(num_jobs, check):
             "Company Name" : company_name,
             "Job Description" : job_description})
 
+            try:
+                driver.find_element(By.CLASS_NAME, 'Next').click()
+            except NoSuchElementException:
+                print("Scraping terminated because reached limit.")
+                break
+
     return pd.DataFrame(jobs)
 
-
+data = load_iris()
 df = python_jobs(5, False)
+
 print(df.to_string()) 
