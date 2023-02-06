ff5c45ba1e106d775eb7c3913f3e4d1974820d13
almon-s <almon.subba@gmail.com>
2023-02-06 15:29:11 +0800

    Update.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import pandas as pd
 from sklearn.datasets import load_iris
 
+
 def python_jobs(num_jobs, check):
     '''Gets jobs for 'python developer' as a dataframe from Glassdoor.'''
 
@@ -17,41 +18,44 @@ def python_jobs(num_jobs, check):
     # If its true, will still be looking for jobs.
     while len(jobs) < num_jobs:
 
-        time.sleep(4)
+        time.sleep(5)
 
-        # For the sign up prompt, get rid of it.
-        try:
-            driver.find_element(By.CLASS_NAME, 'selected').click()
-        except ElementClickInterceptedException:
-            pass
-
-            time.sleep(.1)
-
-        # Clicking the 'X' button.
+        # Close sign up prompt pop up.
         try:
             driver.find_element(By.CLASS_NAME, 'SVGInline-svg modal_closeIcon-svg').click()
         except NoSuchElementException:
             pass
 
+        time.sleep(1)
+
+        # Expand description by clicking See More.
+        try:
+            driver.find_element(By.CSS_SELECTOR, 'css-t3xrds e856ufb4').click()
+        except NoSuchElementException:
+            pass
+
 
         # Clicking each job card button.
-        job_buttons = driver.find_element(By.CLASS_NAME, 'jl')
+        job_buttons = driver.find_elements(By.XPATH, "//article[@id='MainCol']//ul/li[@data-adv-type='GENERAL']")
         for job_button in job_buttons:
-            print("Loading...")
             
+            print("Loading...")
             if len(jobs) >= num_jobs:
                 break
 
-            job_button.click()
-            time.sleep(1)
-            collected_successfully = False
+            try:
+                job_button.click()
+                time.sleep(1)
+                collected_successfully = False
+            except ElementClickInterceptedException:
+                pass
             
             # Keeps searching unless.
             while not collected_successfully:
                 try:
-                    company_name = driver.find_element(By.XPATH, '//div[@data-test="employerName"]').text
-                    job_title = driver.find_element(By.XPATH, '//div[@data-test="jobTitle"]').text
-                    job_description = driver.find_element(By.XPATH, '//div[@class="jobDescriptionContent desc"]').text
+                    company_name = driver.find_element(By.XPATH, './/div[@class="css-87uc0g e1tk4kwz1"]').text
+                    job_title = driver.find_element(By.XPATH, './/div[@class="css-1vg6q84 e1tk4kwz4"]').text
+                    job_description = driver.find_element(By.XPATH, './/div[@class="jobDescriptionContent desc"]').text
                     collected_successfully = True
                 except:
                     time.sleep(5)
@@ -65,15 +69,13 @@ def python_jobs(num_jobs, check):
             "Company Name" : company_name,
             "Job Description" : job_description})
 
-            try:
-                driver.find_element(By.CLASS_NAME, 'Next').click()
-            except NoSuchElementException:
-                print("Scraping terminated because reached limit.")
-                break
+        try:
+            driver.find_element(By.CLASS_NAME, 'SVGInline-svg navIcon-svg css-g9i05x-svg e13qs2073-svg').click()
+        except NoSuchElementException:
+            print("Scraping terminated because reached limit.")
+            break
 
     return pd.DataFrame(jobs)
 
-data = load_iris()
 df = python_jobs(5, False)
-
-print(df.to_json())
+print(df.to_string())
